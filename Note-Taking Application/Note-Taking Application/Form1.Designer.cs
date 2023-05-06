3d2a228868326c080d9a58c4ae2db69804438250
Hasan Göynücek <78232847+Goynucek@users.noreply.github.com>
2023-05-06 23:12:09 +0300

    Add files via upload

---
+++ Note-Taking Application/Note-Taking Application/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Note_Taking_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Message = new System.Windows.Forms.RichTextBox();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.TodoList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReadBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.Snow;
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Title.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Title.Location = new System.Drawing.Point(6, 19);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(472, 20);
+            this.Title.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Title);
+            this.groupBox1.Location = new System.Drawing.Point(36, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 50);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Title";
+            // 
+            // Message
+            // 
+            this.Message.BackColor = System.Drawing.Color.Snow;
+            this.Message.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Message.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Message.Location = new System.Drawing.Point(36, 80);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(485, 307);
+            this.Message.TabIndex = 19;
+            this.Message.Text = "";
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(36, 393);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(485, 23);
+            this.SaveBtn.TabIndex = 20;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // TodoList
+            // 
+            this.TodoList.BackColor = System.Drawing.Color.Snow;
+            this.TodoList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TodoList.FormattingEnabled = true;
+            this.TodoList.Location = new System.Drawing.Point(581, 84);
+            this.TodoList.Name = "TodoList";
+            this.TodoList.Size = new System.Drawing.Size(207, 301);
+            this.TodoList.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Lists:";
+            // 
+            // ReadBtn
+            // 
+            this.ReadBtn.Location = new System.Drawing.Point(581, 392);
+            this.ReadBtn.Name = "ReadBtn";
+            this.ReadBtn.Size = new System.Drawing.Size(99, 23);
+            this.ReadBtn.TabIndex = 23;
+            this.ReadBtn.Text = "Read";
+            this.ReadBtn.UseVisualStyleBackColor = true;
+            this.ReadBtn.Click += new System.EventHandler(this.ReadBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(686, 392);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(102, 23);
+            this.DeleteBtn.TabIndex = 24;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.ReadBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TodoList);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox Message;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.ListBox TodoList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ReadBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+    }
+}
+
---
+++ Note-Taking Application/Note-Taking Application/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Note_Taking_Application
+{
+    public partial class Form1 : Form
+    {
+
+        XmlDocument xmlDoc = new XmlDocument();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            if(File.Exists("todo.xml"))
+            {
+                xmlDoc.Load("todo.xml");
+                XmlNodeList nodes = xmlDoc.SelectNodes("/TodoList/Item/*");
+
+                foreach (XmlNode item in nodes)
+                {
+                    TodoList.Items.Add(item.Attributes["Title"].Value);
+                }
+            }
+        }
+
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if(Title.Text != "")
+            {
+                if (!File.Exists("todo.xml")) //File Yoksa
+                {
+                    TodoList.Items.Add(Title.Text);
+                    XmlReader();
+                }
+                else //File Varsa
+                {
+                    xmlDoc.Load("todo.xml");
+                    XmlNodeList nodes = xmlDoc.SelectNodes("/TodoList/Item/*");
+
+                    if(TodoList.Items.Count != 0)
+                    {
+                        foreach (XmlNode item in nodes)
+                        {
+                            if (Title.Text == item.Attributes["Title"].Value)
+                            {
+                                XmlUpdate(item);
+                            }
+                            else
+                            {
+                                XmlAdd();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        XmlAdd();
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Title Cannot Be Empty","Alert");
+            }
+
+        }
+
+        public void XmlAdd()
+        {
+            xmlDoc.Load("todo.xml");
+
+            XmlElement Todo = xmlDoc.CreateElement("Todo");
+            XmlAttribute name = xmlDoc.CreateAttribute("Title");
+            name.Value = Title.Text;
+            Todo.Attributes.Append(name);
+
+            XmlElement message = xmlDoc.CreateElement("Message");
+            message.InnerText = Message.Text;
+
+            XmlNode todo = xmlDoc.SelectSingleNode("/TodoList/Item");
+            Todo.AppendChild(message);
+            todo.AppendChild(Todo);
+
+            TodoList.Items.Add(Title.Text);
+            xmlDoc.Save("todo.xml");
+        }
+
+        public void XmlUpdate(XmlNode node)
+        {
+            node.FirstChild.InnerText = Message.Text;
+            xmlDoc.Save("todo.xml");
+        }
+
+        public void XmlReader()
+        {
+            XmlElement TodoList = xmlDoc.CreateElement("TodoList");
+            xmlDoc.AppendChild(TodoList);
+
+            XmlElement item = xmlDoc.CreateElement("Item");
+            TodoList.AppendChild(item);
+
+            XmlElement Todo = xmlDoc.CreateElement("Todo");
+            item.AppendChild(Todo);
+            XmlAttribute name = xmlDoc.CreateAttribute("Title");
+            name.Value = Title.Text;
+            Todo.Attributes.Append(name);
+
+            XmlElement message = xmlDoc.CreateElement("Message");
+            Todo.AppendChild(message);
+            message.InnerText = Message.Text;
+
+            xmlDoc.Save("todo.xml");
+
+            //<TodoList>
+            //  <item>
+                    //<Todo title="">
+            //          <Message> sgsdgsdgsdgsdg </Message>
+                    //</Todo>
+            //   </item>
+            //</TodoList>
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            xmlDoc.Load("todo.xml");
+            XmlNodeList nodes = xmlDoc.SelectNodes("/TodoList/Item/*");
+
+            foreach (XmlNode item in nodes)
+            {
+                if (item.Attributes["Title"].Value == TodoList.SelectedItem.ToString())
+                {
+                    item.ParentNode.RemoveChild(item);
+                    xmlDoc.Save("todo.xml");
+                }
+            }
+
+            TodoList.Items.Remove(TodoList.SelectedItem);
+        }
+
+        private void ReadBtn_Click(object sender, EventArgs e)
+        {
+            string a = TodoList.SelectedItem.ToString();
+            Title.Text = a;
+            xmlDoc.Load("todo.xml");
+            XmlNodeList nodes = xmlDoc.SelectNodes("/TodoList/Item/*");
+            foreach (XmlNode item in nodes)
+            {
+                string b = item.Attributes["Title"].Value;
+                if (b == a)
+                {
+                    Message.Text = item.FirstChild.InnerText;
+                }
+            }
+        }
+    }
+}
